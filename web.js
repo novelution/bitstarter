83d272d04a18119349dc6430b7ca3c2eb8906dbf
Yaniv <yaniv@novelution.com>
2013-07-21 20:27:07 -0400

    Changed port

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var content = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    content = fs.readFileSync('index.html')
+    response.send(content.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
